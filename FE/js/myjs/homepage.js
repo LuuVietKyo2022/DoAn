f9dba91e15b516be1cb7c8b28b5958d43b57e99c
VietLVlaptop <luuviet.ln1bg@gmail.com>
2024-03-04 21:58:59 +0700

    update VietLV11 update homepage 1.2

--- FE/js/myjs/homepage.js
+++ FE/js/myjs/homepage.js
@@ -10,6 +10,43 @@
         inputCreabtnCloseStatusFormteStatus.click();
     })
 
-    modalCreateStatus.onclick(function(){
-        modalCreateStatus.hide();
+   // modalCreateStatus.onclick(function(){
+    //    modalCreateStatus.hide();
+    //})
+    function clickHeart(){
+        const btnLightHeart=document.getElementById("btn-heart");
+        const iLightHeart=document.getElementById("i-lightheart");
+        if(iLightHeart.classList.contains("bi-heart")){
+            iLightHeart.classList.add('bi-heart-fill');
+            iLightHeart.classList.remove('bi-heart');
+            iLightHeart.style.color="red";
+        }else{
+            iLightHeart.classList.add('bi-heart');
+            iLightHeart.classList.remove('bi-heart-fill');
+            iLightHeart.style.color="black";
+        }
+        
+    }
+
+    function inputComment(){
+        const inputComment=document.getElementById("my-inputcomment");
+        const iSend=document.getElementById("i-send");
+        if(inputComment.value!=''){
+            iSend.classList.add('bi-send-fill');
+            iSend.classList.remove('bi-send');
+            iSend.style.color="blue";
+        }else{
+            iSend.classList.add('bi-send');
+            iSend.classList.remove('bi-send-fill');
+            iSend.style.color="black";
+        }
+    }
+    const btnCreateComment=document.getElementById("btn-create-comment");
+    const inputComment2=document.getElementById("my-inputcomment");
+    btnCreateComment.addEventListener('click',()=>{
+        inputComment2.focus();
     })
+   
+
+    
+   

e156fab9c467338e8e35de8ce4130eef4fc129e3
VietLVlaptop <luuviet.ln1bg@gmail.com>
2024-03-12 16:29:06 +0700

    update Update addinforpage

---
+++ FE/js/myjs/addinforpage.js
@@ -0,0 +1,9 @@
+function createUserName(){
+    const firstName=document.getElementById('firstName').value;
+    const lastName=document.getElementById('lastName').value;
+    const firstNameLastName=document.getElementById('firstNameLastName');
+    const lastNameFirstName=document.getElementById('lastNameFirstName');
+    firstNameLastName.value=firstName+lastName;
+    lastNameFirstName.value=lastName+firstName;
+
+}
